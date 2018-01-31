f03582bbe59ae983c49646c8557a29a6fbaae53b
Tomáš Kolárik <kolarto5@fit.cvut.cz>
2018-01-31 23:07:43 +0100

    Receiving message binary data corrected. Messages A, H, M tested, but now stucked at RSpec failure due to strings inequality despite their content (e.g. #chars) is the same.. This is likely related to Base64 encoding/decoding, I have replicated this "bug" in irb likewise: `Base64.decode64('bcde') == "m\xC7^"` .. is false

--- semestral_work/messages.rb
+++ semestral_work/messages.rb
@@ -7,12 +7,23 @@ module MaxCube
   # * Ends with '\r\n'
   # Example (unencoded):
   # X:message\r\n
+  # As all messages are being split by '\r\n',
+  # it does not occur in single message processing,
+  # only in raw data processing
   class MessageHandler
+    # Without '\r\n', with it it is 1900
+    MSG_MAX_LEN = 1898
 
     class InvalidMessage < RuntimeError; end
 
     class InvalidMessageEmpty < InvalidMessage
       def initialize(info = 'empty message')
+        super
+      end
+    end
+
+    class InvalidMessageLength < InvalidMessage
+      def initialize(info = 'invalid message length')
         super
       end
     end
@@ -35,12 +46,21 @@ module MaxCube
       end
     end
 
-    # Check single message validity, ignoring presence of '\r\n' at the end
+    def valid_msg_length(msg)
+      msg.length.between?(2, MSG_MAX_LEN)
+    end
+
+    def check_msg_length(msg)
+      raise InvalidMessageLength unless valid_msg_length(msg)
+    end
+
+    # Check single message validity, already without '\r\n' at the end
     def valid_msg(msg)
-      msg =~ /^[[:alpha:]]:[^:]*$/
+      valid_msg_length(msg) && msg =~ /^[[:alpha:]]:[^:]*$/
     end
 
     def check_msg(msg)
+      check_msg_length(msg)
       raise InvalidMessageFormat unless valid_msg(msg)
     end
 
@@ -67,7 +87,7 @@ module MaxCube
       check_empty_data(raw_data)
     end
 
-    def valid_lengths(lengths, *args)
+    def valid_msg_part_lengths(lengths, *args)
       return false if args.any?(&:nil?) ||
         args.length < lengths.length
       args.each_with_index.all? do |v, i|
@@ -75,28 +95,35 @@ module MaxCube
       end
     end
 
-    def check_lengths(msg_type, lengths, *args)
-      return if valid_lengths(lengths, *args)
+    def check_msg_part_lengths(msg_type, lengths, *args)
+      return if valid_msg_part_lengths(lengths, *args)
       raise InvalidMessageBody.new(msg_type,
         "invalid lengths of message parts #{args} (lengths should be: #{lengths})")
     end
 
-    def valid_types(type, *args)
-      args.all? { |x| x.is_a?(type) }
+    # Convert string of characters (not binary data!) to hex number
+    # For binary data use #String.unpack
+    def hex_to_i_check(msg_type, info, *args)
+      raise InvalidMessageBody.new(msg_type,
+        "invalid hex format of message parts #{args} (#{info})") unless args.all? do |x|
+          x && !x[/\H/] && !x[/^$/]
+        end
+      args.map { |x| x.to_i(16) }
     end
 
-    def check_types(msg_type, type, *args)
-      return if valid_types(type, *args)
-      raise InvalidMessageBody.new(msg_type,
-        "invalid types of message parts #{args} (type should be: #{type})")
+    def check_msg_min_data_length(msg_type, min_length, length, info)
+      return unless length < min_length
+      raise InvalidMessageBody.new(msg_type, "#{info} - remaining data length is insufficient (#{length} < #{min_length})")
     end
 
     def encode(data)
-      Base64.encode64(data)
+      # Base64.encode64(data)
+      Base64.strict_encode64(data)
     end
 
     def decode(data)
-      Base64.decode64(data)
+      # Base64.decode64(data)
+      Base64.strict_decode64(data)
     end
 
   end
@@ -127,6 +154,7 @@ module MaxCube
 
     # Process set of messages - raw data separated by '\r\n'
     # Returns array of hashes
+    #! Dokumentovat vstup jako String a vyradit type checking
     def recv_data(raw_data)
       check_data(raw_data)
       raw_data.split("\r\n").map(&method(:recv_msg)).to_a
@@ -154,11 +182,14 @@ module MaxCube
       values = body.split(',')
       # lengths = [10, 6, 4, 8, 8, 2, 2, 6, 4, 2, 4]
       lengths = [10, 6, 4]
-      check_lengths(msg_type, lengths, *values)
+      check_msg_part_lengths(msg_type, lengths, *values)
+      values[2] = hex_to_i_check(msg_type, 'firmware version', values[2])[0]
+      # values[2] = values[2].unpack('N')[0]
       keys = %i[
         serial_number
         rf_address
         firmware_version
+      ]
         # unknown
         # http_id
         # duty_cycle
@@ -167,7 +198,6 @@ module MaxCube
         # cube_time
         # state_cube_time
         # ntp_counter
-      ]
       keys.zip(values).to_h
     end
 
@@ -175,24 +205,42 @@ module MaxCube
     def recv_msg_m(msg_type, body)
       index, count, enc_data = body.split(',')
       lengths = [2, 2]
-      check_lengths(msg_type, lengths, index, count)
-      [index, count].map(&:to_i)
-      check_types(msg_type, Integer, index, count)
+      check_msg_part_lengths(msg_type, lengths, index, count)
+      index, count = hex_to_i_check(msg_type, 'message index, count', index, count)
+      # index, count = [index, count].map { |x| x.unpack('n')[0] }
+      raise InvalidMessageBody.new(msg_type, "index >= count: #{index} >= #{count}") unless index < count
 
+      raise InvalidMessageBody.new(msg_type, "message data is missing") unless enc_data
       data_io = StringIO.new(decode(enc_data))
+      check_msg_min_data_length(msg_type, 4, data_io.size, 'rooms/devices')
+      
       hash = { index: index, count: count }
+      # hash[:unknown] = data_io.read(2)
+      hash[:unknown] = data_io.read(2).unpack1('Z*')
+      # a = hash[:unknown]
+      # hash[:unknown] = "V\x02"
+      # hash[:unknown] = "V\u0002"
+      # p a
+      # p hash[:unknown]
+      # p a == hash[:unknown]
+      # p ''
 
       # Rooms
-      hash[:rooms_unknown] = data_io.read(2)
-      rooms_count = data_io.read(1).to_i
+      # rooms_count = hex_to_i_check(msg_type, 'rooms count', data_io.read(1))[0]
+      rooms_count = data_io.read(1).unpack1('C')
+      rooms_min_length = rooms_count * 5
+      check_msg_min_data_length(msg_type, rooms_min_length, data_io.size - data_io.pos, 'rooms')
       hash[:rooms_count] = rooms_count
       # hash[:rooms] = {}
       hash[:rooms] = []
       rooms_count.times do
         room = {}
-        room_id = data_io.read(1).to_i
+        # room_id = hex_to_i_check(msg_type, 'room ID', data_io.read(1))[0]
+        room_id = data_io.read(1).unpack1('C')
         room[:id] = room_id
-        room_name_length = data_io.read(1).to_i
+        # room_name_length = hex_to_i_check(msg_type, 'room name length', data_io.read(1))[0]
+        room_name_length = data_io.read(1).unpack1('C')
+        check_msg_min_data_length(msg_type, room_name_length + 3, data_io.size - data_io.pos, 'room')
         room[:name_length] = room_name_length
         room[:name] = data_io.read(room_name_length)
         room[:rf_address] = data_io.read(3)
@@ -202,18 +250,26 @@ module MaxCube
       end
 
       # Devices
-      devices_count = data_io.read(1).to_i
+      check_msg_min_data_length(msg_type, 1, data_io.size - data_io.pos, 'devices')
+      # devices_count = hex_to_i_check(msg_type, 'devices count', data_io.read(1))[0]
+      devices_count = data_io.read(1).unpack1('C')
+      devices_min_length = devices_count * 16
+      check_msg_min_data_length(msg_type, devices_min_length, data_io.size - data_io.pos, 'devices')
       hash[:devices_count] = devices_count
       hash[:devices] = []
       devices_count.times do
         device = {}
-        device[:type] = data_io.read(1).to_i
+        # device[:type] = hex_to_i_check(msg_type, 'device type', data_io.read(1))[0]
+        device[:type] = data_io.read(1).unpack1('C')
         device[:rf_address] = data_io.read(3)
         device[:serial_number] = data_io.read(10)
-        device_name_length = data_io.read(1).to_i
+        # device_name_length = hex_to_i_check(msg_type, 'device name length', data_io.read(1))[0]
+        device_name_length = data_io.read(1).unpack1('C')
+        check_msg_min_data_length(msg_type, device_name_length + 1, data_io.size - data_io.pos, 'device')
         device[:name_length] = device_name_length
         device[:name] = data_io.read(device_name_length)
-        device[:room_id] = data_io.read(1).to_i
+        # device[:room_id] = hex_to_i_check(msg_type, "device's room ID", data_io.read(1))[0]
+        device[:room_id] = data_io.read(1).unpack1('C')
 
         hash[:devices] << device
       end
@@ -230,4 +286,13 @@ end
 
 # MaxCube::MessageReceiver.new.recv_data('')
 # MaxCube::MessageReceiver.new.recv_data(':')
-MaxCube::MessageReceiver.new.recv_data('H:')
+# MaxCube::MessageReceiver.new.recv_data('H:')
+# MaxCube::MessageReceiver.new.recv_data('M:00,01,')
+# MaxCube::MessageReceiver.new.recv_data('M:00,01,'+Base64.encode64('abcd'))
+# p MaxCube::MessageReceiver.new.recv_data('M:00,01,'+Base64.encode64('ab192XY123'))
+# p MaxCube::MessageReceiver.new.recv_data('M:00,01,'+Base64.encode64('ab192XY1230'))
+# p MaxCube::MessageReceiver.new.recv_data('M:00,01,'+Base64.encode64("ab\x01!\x02XY123\x00"))
+# p MaxCube::MessageReceiver.new.recv_data("M:00,01,VgIEAQNCYWQK7WkCBEJ1cm8K8wADCldvaG56aW1tZXIK8wwEDFNjaGxhZnppbW1lcgr
+# 1QAUCCu1pS0VRMDM3ODA0MAZIVCBCYWQBAgrzAEtFUTAzNzk1NDQHSFQgQnVybwICCvMMS0VRMD
+# M3OTU1NhlIVCBXb2huemltbWVyIEJhbGtvbnNlaXRlAwIK83lLRVEwMzc5NjY1GkhUIFdvaG56a
+# W1tZXIgRmVuc3RlcnNlaXRlAwIK9UBLRVEwMzgwMTIwD0hUIFNjaGxhZnppbW1lcgQB\r\n")
--- semestral_work/spec/msg_spec.rb
+++ semestral_work/spec/msg_spec.rb
@@ -1,12 +1,17 @@
+require 'base64'
 require_relative '../messages'
 require_relative 'spec_helper'
 
 describe 'MessageReceiver' do
   subject(:recv) { MaxCube::MessageReceiver.new }
-  # let(:game) do
-  #   '003020600900305001001806400008102900700' \
-  #   '000008006708200002609500800203009005010300'
-  # end
+
+  # Proper messages:
+  # A:\r\n
+  # H:KEQ0523864,097f2c,0113,00000000,477719c0,00,32,0d0c09,1404,03,0000\r\n
+  # M:00,01,VgIEAQNCYWQK7WkCBEJ1cm8K8wADCldvaG56aW1tZXIK8wwEDFNjaGxhZnppbW1lcgr
+  #   1QAUCCu1pS0VRMDM3ODA0MAZIVCBCYWQBAgrzAEtFUTAzNzk1NDQHSFQgQnVybwICCvMMS0VRMD
+  #   M3OTU1NhlIVCBXb2huemltbWVyIEJhbGtvbnNlaXRlAwIK83lLRVEwMzc5NjY1GkhUIFdvaG56a
+  #   W1tZXIgRmVuc3RlcnNlaXRlAwIK9UBLRVEwMzgwMTIwD0hUIFNjaGxhZnppbW1lcgQB\r\n
 
   describe 'invalid message' do
     context 'empty message' do
@@ -39,10 +44,10 @@ describe 'MessageReceiver' do
       context 'of single message' do
         let(:msgs) do
           [
-            ':',
             '::',
             'H::',
             'H:X:',
+            'A:A:',
             'HX:',
             'HX:A',
             'HX:A:',
@@ -51,8 +56,13 @@ describe 'MessageReceiver' do
         end
         it 'raises proper exception and #valid_recv_msg is falsey' do
           msgs.each do |m|
-            expect { recv.recv_data(m) }.to raise_error MaxCube::MessageHandler::InvalidMessageFormat
+            expect { recv.recv_msg(m) }.to raise_error MaxCube::MessageHandler::InvalidMessageFormat
             expect(recv.valid_recv_msg(m)).to be_falsey
+          end
+        end
+        it 'raises proper exception when passed as raw data' do
+          msgs.each do |m|
+            expect { recv.recv_data(m) }.to raise_error MaxCube::MessageHandler::InvalidMessageFormat
           end
         end
       end
@@ -60,8 +70,13 @@ describe 'MessageReceiver' do
       context 'of raw data' do
         let(:data) do
           [
-            "A:\r\naX:",
+            'A:\r\nA:',
+            'A:\nA:',
+            'A:\rA:',
+            'A:\r\nA:\r\n',
+            "A:A:\r\n",
             "A:\r\nA:\r\naX:",
+            "A:\r\nA:\r\naX:\r\n",
             "H:KEQ0523864,097f2c,0113,00000000,477719c0,00,32,0d0c09,1404,03,0000\r\nA:\r\nA:\r\naX:",
             "A:\r\nH:KEQ0523864,097f2c,0113,00000000,477719c0,00,32,0d0c09,1404,03,0000\r\nA:\r\naX:",
             "A:\nH:KEQ0523864,097f2c,0113,00000000,477719c0,00,32,0d0c09,1404,03,0000\r\nA:\r\naX:",
@@ -103,6 +118,220 @@ describe 'MessageReceiver' do
       it 'raises proper exception' do
         msgs.each do |m|
           expect { recv.recv_data(m) }.to raise_error MaxCube::MessageHandler::InvalidMessageBody
+        end
+      end
+    end
+
+    context 'invalid message length' do
+      let(:msgs) do
+        [
+          'A',
+          ':',
+          'A:' + 'x' * MaxCube::MessageHandler::MSG_MAX_LEN,
+        ]
+      end
+      it 'raises proper exception and #valid_msg_length is falsey' do
+        msgs.each do |m|
+          expect { recv.recv_msg(m) }.to raise_error MaxCube::MessageHandler::InvalidMessageLength
+          expect(recv.valid_msg_length(m)).to be_falsey
+        end
+      end
+    end
+  end
+
+  describe 'valid data' do
+    let(:data) do
+      [
+        "A:\r\nH:KEQ0523864,097f2c,0113,00000000,477719c0,00,32,0d0c09,1404,03,0000\r\n",
+        "A:abcde\r\nA:\r\n",
+        "H:KEQ0523864,097f2c,0113,00000000,477719c0,00,32,0d0c09,1404,03,0000\r\n"\
+        "H:KEQ0523864,097f2c,0113,00000000,477719c0,00,32,0d0c09,1404,03,0000\r\n",
+        "A:\r\nA:\r\n"\
+        "H:KEQ0523864,097f2c,0113,00000000,477719c0,00,32,0d0c09,1404,03,0000\r\n"\
+        "A:\r\n"\
+        "M:00,01,VgIEAQNCYWQK7WkCBEJ1cm8K8wADCldvaG56aW1tZXIK8wwEDFNjaGxhZnppbW1lcgr"\
+        "1QAUCCu1pS0VRMDM3ODA0MAZIVCBCYWQBAgrzAEtFUTAzNzk1NDQHSFQgQnVybwICCvMMS0VRMD"\
+        "M3OTU1NhlIVCBXb2huemltbWVyIEJhbGtvbnNlaXRlAwIK83lLRVEwMzc5NjY1GkhUIFdvaG56a"\
+        "W1tZXIgRmVuc3RlcnNlaXRlAwIK9UBLRVEwMzgwMTIwD0hUIFNjaGxhZnppbW1lcgQB\r\n"\
+        "A:\r\n",
+      ]
+    end
+    let(:res) do
+      [
+        %w[A H],
+        %w[A A],
+        %w[H H],
+        %w[A A H A M A],
+      ]
+    end
+    it 'returns array of hashes with proper message types' do
+      data.each_with_index do |d, i|
+        ary = recv.recv_data(d)
+        expect(ary).to be_an(Array).and all(be_a(Hash))
+        ary.each_with_index do |h, j|
+          expect(h).to include(:type)
+          expect(h[:type]).to eq(res[i][j])
+        end
+      end
+    end
+  end
+
+  describe 'concrete message types' do
+    describe 'A message' do
+      let(:msgs) do
+        [
+          'A:',
+          'A:acknowledgement!',
+          'A:\r\n',
+          'A:1234abcd',
+        ]
+      end
+      it 'ignores any content of message' do
+        msgs.each do |m|
+          expect(recv.recv_msg(m)).to eq({ type: 'A' })
+        end
+      end
+    end
+
+    # H:KEQ0523864,097f2c,0113,00000000,477719c0,00,32,0d0c09,1404,03,0000
+    describe 'H message' do
+      context 'invalid message body' do
+        let(:msgs) do
+          [
+            # invalid lengths of message parts
+            'H:',
+            'H:,,,,,,,,,,',
+            'H:EQ0523864,097f2c,0113,00000000,477719c0,00,32,0d0c09,1404,03,0000',
+            'H:IKEQ0523864,097f2c,0113,00000000,477719c0,00,32,0d0c09,1404,03,0000',
+            'H:IEQ0523864,97f2c,0113,00000000,477719c0,00,32,0d0c09,1404,03,0000',
+            'H:IKEQ0523864,97f2c,0113,00000000,477719c0,00,32,0d0c09,1404,03,0000',
+            'H:IEQ0523864,197f2c,113,00000000,477719c0,00,32,0d0c09,1404,03,0000',
+            'H:IKEQ0523864,197f2c,113,00000000,477719c0,00,32,0d0c09,1404,03,0000',
+            # invalid hex format of 3rd part
+            'H:KEQ0523864,097f2c,x113,00000000,477719c0,00,32,0d0c09,1404,03,0000',
+          ]
+        end
+        it 'raises proper exception' do
+          msgs.each do |m|
+            expect{ recv.recv_msg(m) }.to raise_error MaxCube::MessageHandler::InvalidMessageBody
+          end
+        end
+      end
+
+      context 'valid message body' do
+        let(:msgs) do
+          [
+            'H:KEQ0523864,097f2c,0113,00000000,477719c0,00,32,0d0c09,1404,03,0000',
+            'H:4KFK49VMD6,c233fe,211f,00000000,477719c0,00,32,0d0c09,1404,03,0000',
+          ]
+        end
+        let(:ret) do
+          [
+            { type: 'H', serial_number: 'KEQ0523864', rf_address: '097f2c', firmware_version: '0113'.to_i(16) },
+            { type: 'H', serial_number: '4KFK49VMD6', rf_address: 'c233fe', firmware_version: '211f'.to_i(16) },
+          ]
+        end
+        it 'returns proper hash' do
+          msgs.each_with_index do |m, i|
+            expect(recv.recv_msg(m)).to eq(ret[i])
+          end
+        end
+      end
+    end
+
+    # M:00,01,VgIEAQNCYWQK7WkCBEJ1cm8K8wADCldvaG56aW1tZXIK8wwEDFNjaGxhZnppbW1lcgr
+    #   1QAUCCu1pS0VRMDM3ODA0MAZIVCBCYWQBAgrzAEtFUTAzNzk1NDQHSFQgQnVybwICCvMMS0VRMD
+    #   M3OTU1NhlIVCBXb2huemltbWVyIEJhbGtvbnNlaXRlAwIK83lLRVEwMzc5NjY1GkhUIFdvaG56a
+    #   W1tZXIgRmVuc3RlcnNlaXRlAwIK9UBLRVEwMzgwMTIwD0hUIFNjaGxhZnppbW1lcgQB
+    describe 'M message' do
+      context 'invalid message body' do
+        let(:msgs) do
+          [
+            # message parts lengths are too short
+            'M:00',
+            'M:00,',
+            'M:00,,',
+            'M:00,01',
+            'M:00,01,',
+            'M:00,01,' + Base64.strict_encode64('ab1'),
+            'M:00,01,' + Base64.strict_encode64('ab01'),
+            'M:00,01,' + Base64.strict_encode64('ab192XY123'),
+            'M:00,01,' + Base64.strict_encode64("ab\x01"),
+            'M:00,01,' + Base64.strict_encode64("ab\x00\x01"),
+            'M:00,01,' + Base64.strict_encode64("ab\x01!\x02XY123"),
+            'M:00,01,' + Base64.strict_encode64("ab\x01!\x02XY123\x01"),
+            'M:00,01,' + Base64.strict_encode64("ab\x01!\x02XY123\x01\x04RFAserial_num\x04NAME"),
+            'M:00,01,' + Base64.strict_encode64("ab\x01!\x02XY123\x02\x04RFAserial_num\x04NAME!"),
+            # index/count not hex
+            'M:xx,01,' + Base64.strict_encode64("ab\x00\x00"),
+            'M:00,xx,' + Base64.strict_encode64("ab\x00\x00"),
+            # index >= count
+            'M:01,01,' + Base64.strict_encode64("ab\x00\x00"),
+          ]
+        end
+        it 'raises proper exception' do
+          msgs.each do |m|
+            expect{ recv.recv_msg(m) }.to raise_error MaxCube::MessageHandler::InvalidMessageBody
+          end
+        end
+      end
+
+      context 'valid message body' do
+        let(:msgs) do
+          [
+            'M:00,01,' + Base64.strict_encode64("V\x02\x01!\x02XY123\x01\x04RFAserial_num\x04NAME!"),
+            'M:00,01,VgIEAQNCYWQK7WkCBEJ1cm8K8wADCldvaG56aW1tZXIK8wwEDFNjaGxhZnppbW1lcgr' \
+              '1QAUCCu1pS0VRMDM3ODA0MAZIVCBCYWQBAgrzAEtFUTAzNzk1NDQHSFQgQnVybwICCvMMS0VRMD' \
+              'M3OTU1NhlIVCBXb2huemltbWVyIEJhbGtvbnNlaXRlAwIK83lLRVEwMzc5NjY1GkhUIFdvaG56a' \
+              'W1tZXIgRmVuc3RlcnNlaXRlAwIK9UBLRVEwMzgwMTIwD0hUIFNjaGxhZnppbW1lcgQB',
+          ]
+        end
+        let(:ret) do
+          [
+            {
+              type: 'M', index: 0, count: 1, unknown: "V\x02",
+              rooms_count: 1, rooms: [
+                { id: '!'.unpack1('C'), name_length: 2, name: 'XY', rf_address: '123'},
+              ],
+              devices_count: 1, devices: [
+                { type: 4, rf_address: 'RFA', serial_number: 'serial_num', name_length: 4, name: 'NAME', room_id: '!'.unpack1('C') },
+              ],
+            },
+            {
+              type: 'M', index: 0, count: 1, unknown: "V\x02",
+              rooms_count: 4, rooms: [
+                { id: 1, name_length: 3, name: 'Bad', rf_address: "\x0a\xed\x69" },
+                { id: 2, name_length: 4, name: 'Buro', rf_address: "\x0a\xf3\x00" },
+                { id: 3, name_length: 10, name: 'Wohnzimmer', rf_address: "\x0a\xf3\x0c" },
+                { id: 4, name_length: 12, name: 'Schlafzimmer', rf_address: "\x0a\xf5\x40" },
+              ],
+              devices_count: 5, devices: [
+                { type: 2, rf_address: "\x0a\xed\x69", serial_number: 'KEQ0378040', name_length: 6, name: 'HT Bad', room_id: 1 },
+                { type: 2, rf_address: "\n\xF3\x00", serial_number: 'KEQ0379544', name_length: 7, name: 'HT Buro', room_id: 2 },
+                { type: 2, rf_address: "\n\xF3\f", serial_number: 'KEQ0379556', name_length: 25, name: 'HT Wohnzimmer Balkonseite', room_id: 3 },
+                { type: 2, rf_address: "\n\xF3y", serial_number: 'KEQ0379665', name_length: 26, name: 'HT Wohnzimmer Fensterseite', room_id: 3 },
+                { type: 2, rf_address: "\n\xF5@", serial_number: 'KEQ0380120', name_length: 15, name: 'HT Schlafzimmer', room_id: 4},
+              ],
+            },
+          ]
+        end
+        # let(:test) { [{a:"V\x02"}] }
+        # let(:test2) { [{a:"V\u0002"}] }
+        it 'returns proper hash' do
+          msgs.each_with_index do |m, i|
+            expect(recv.recv_msg(m)).to eq(ret[i])
+            # expect(ret[i]).to eq(ret[i])
+            # expect(recv.recv_msg(m)).to eql(ret[i])
+            # expect("V\x02").to eq("V\u0002")
+            # expect({a:"V\u0002"}).to eq({a:"V\x02"})
+            # expect(test).to eq("V\u0002")
+            # expect("V\u0002").to eq(test)
+            # expect(test[0]).to eq(test[0])
+            # expect(test[0]).to eq([{a:"V\x02"}][0])
+            # expect(test[0]).to eq([{a:"V\u0002"}][0])
+            # expect(test[0]).to eq(test2[0])
+            # expect(test2[0]).to eq(test[0])
+          end
         end
       end
     end
